1d1e3d7b1fd4c63c9472b1c08e37cca362831fd1
RFinal3 <rmelanson13@gmail.com>
2026-07-16 02:40:05 -0400

    Added drawing for shots, asteroids and players; added collision detection for asteroids and players

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
 import pygame
+import sys
 from constants import *
+from player import *
 from logger import log_state
+from logger import log_event
+from asteroidfield import *
+from shot import *
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0.0
 
     print(f"Starting Asteroids with pygame version {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+    Shot.containers = (shots, drawable, updatable)
+    asteroid_field = AsteroidField()
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
 
     while True:
@@ -20,8 +39,22 @@ def main():
                 return
 
         screen.fill("black")
+        updatable.update(dt)
+        
+        for obj in asteroids:
+            if obj.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+
+
+        for obj in drawable:
+            obj.draw(screen)
+
 
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
